754535644277bd780e1a2e5ce25f35be7d29e3af
Stefani <sivatgil@gmail.com>
2018-08-02 08:48:33 -0700

    new goal page saves back button gone

---
+++ GoalApp/Controllers/calender.swift
@@ -0,0 +1,31 @@
+////
+////  calender.swift
+////  GoalApp
+////
+////  Created by Stefani İvatgil on 31/07/18.
+////  Copyright © 2018 Stefani İvatgil. All rights reserved.
+////
+//
+//import Foundation
+////import RSDatePickerView.h
+//
+//
+//class CalendarViewController: UIViewController {
+//    @IBOutlet weak var DatePicker: UIDatePicker!
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+////        RSDFDatePickerView *datePickerView = [[RSDFDatePickerView alloc] initWithFrame:self.view.bounds];
+////        datePickerView.delegate = self;
+////        datePickerView.dataSource = self;
+////        [self.view addSubview:datePickerView];
+//    }
+//    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//        
+//    }
+//}
